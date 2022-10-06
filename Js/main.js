6554d1a2dd9f3d779d0915d29bbb601373ed4041
asadbekded <asadbek171@gmail.com>
2022-10-06 12:22:54 +0500

    Add new change

--- Js/main.js
+++ Js/main.js
@@ -5,94 +5,90 @@ const elStr1 = document.querySelector('.js-str1');
 const elStr2 = document.querySelector('.js-str2');
 const elStr3 = document.querySelector('.js-str3');
 
-const todos = [];
+const todos = []
 
-let renderTodos = (array, node) => {
-   node.innerHTML = ''
+let renderTodos = (array, list) => {
+   list.innerHTML = ''
    array.forEach((item) => {
-      let newItem = document.createElement('li')
-      let newSpan = document.createElement('span')
-      let newButton = document.createElement('button')
-      let newInput = document.createElement('input')
+      let newItem = document.createElement('li');
+      let newSpan = document.createElement('span');
+      let newInput = document.createElement('input');
+      let newBtn = document.createElement('button');
 
-      newSpan.textContent = item.text
-      newInput.type = 'checkbox'
-      newButton.textContent = 'Delete'
+      newSpan.textContent = item.text;
+      newInput.type = 'checkbox';
+      newBtn.textContent = 'Delete'
 
       newItem.appendChild(newInput)
       newItem.appendChild(newSpan)
-      newItem.appendChild(newButton)
-      node.appendChild(newItem)
+      newItem.appendChild(newBtn)
+      list.appendChild(newItem)
 
-      newItem.setAttribute('class', 'd-flex align-items-center mb-2 mt-3')
-      newButton.setAttribute('class', 'ms-4 btn btn-danger js-btn')
-      newInput.setAttribute('class', 'form-check-input me-4 js-checkbox')
+      newItem.setAttribute('class', 'd-flex align-items-center mt-3 mb-3')
+      newBtn.setAttribute('class', 'btn btn-danger ms-4 js-btn')
+      newInput.setAttribute('class', 'form-check-input me-4 js-checked')
+      newSpan.setAttribute('class', 'text-info')
 
-      newButton.dataset.todoId = item.id
+      newBtn.dataset.todoId = item.id
       newInput.dataset.todoId = item.id
 
-      if(item.isComplated){
-         newSpan.setAttribute('class', 'text-decoration-line-through')
+      if(item.isComplate){
+         newSpan.setAttribute('class', 'text-decoration-line-through text-danger')
          newInput.checked = true;
       }
-   });
+   })
 }
 
-elForm.addEventListener('submit', (evt) => {
-   evt.preventDefault()
-   let elInpVal = elInp.value;
+elForm.addEventListener('submit', function(evt) {
+   evt.preventDefault();
+   elInpVal = elInp.value
    elInp.value = ''
    todos.push({
-      id: todos.length +1,
+      id: todos.length,
       text: elInpVal,
-      isComplated: false,
+      isComplate: false,
    })
+
    elStr1.textContent = todos.length
 
-   let unComplate = todos.filter((el) => !el.isComplated);
-	elStr3.textContent = unComplate.length;
+   let NoComplate = todos.filter((el) => !el.isComplate)
+   elStr3.textContent = NoComplate.length
 
-   renderTodos(todos,elList)
+
+   renderTodos(todos, elList)
 })
-
 
 elList.addEventListener('click', function(evt) {
    if(evt.target.matches('.js-btn')){
       let todoId = evt.target.dataset.todoId;
-      let findedIndex = todos.findIndex((el) => el.id == todoId);
+      let findedIndex = todos.findIndex((el) => el.id == todoId)
       todos.splice(findedIndex, 1)
 
       elStr1.textContent = todos.length
-      
-      let Complate = todos.filter((el) => el.isComplated);
-		elStr2.textContent = Complate.length;
 
-      let unComplate = todos.filter((el) => !el.isComplated);
-		elStr3.textContent = unComplate.length;
+      let Complate = todos.filter((el) => el.isComplate)
+      elStr2.textContent = Complate.length
+
+      let NoComplate = todos.filter((el) => !el.isComplate)
+      elStr3.textContent = NoComplate.length
 
       renderTodos(todos, elList)
    }
 
-   if(evt.target.matches('.js-checkbox')){
+   if(evt.target.matches('.js-checked')){
       let todoId = +evt.target.dataset.todoId;
-      let findedItem = todos.find((el) => el.id == todoId);
-      findedItem.isComplated = !findedItem.isComplated;
+      let findedItem = todos.find((el) => el.id == todoId)
+      findedItem.isComplate = !findedItem.isComplate
+      
+      let Complate = todos.filter((el) => el.isComplate)
+      elStr2.textContent = Complate.length
 
-      let Complate = todos.filter((el) => el.isComplated);
-		elStr2.textContent = Complate.length;
-
-      let unComplate = todos.filter((el) => !el.isComplated);
-		elStr3.textContent = unComplate.length;
+      let NoComplate = todos.filter((el) => !el.isComplate)
+      elStr3.textContent = NoComplate.length
 
       renderTodos(todos, elList)
    }
 })
-
-
-
-
-
-
 
 
 
